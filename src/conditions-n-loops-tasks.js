4a9917cf64374b2b096066f03afa6e2095beb70a
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-02 20:38:41 +0300

    feat: 4 tests passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -91,8 +91,11 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b < c || b + c < a || c + a < b) return false;
+  if (a === 0 || b === 0 || c === 0) return false;
+  if (a === b || a === c || b === c) return true;
+  return false;
 }
 
 /**
