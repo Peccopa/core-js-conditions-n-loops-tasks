9f60e0c4765c0c7646d01cf6a05ac34216dac772
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-05 20:07:53 +0300

    feat: 10 tests passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -253,8 +253,17 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let result = false;
+  const str = `${num}`;
+  const letter = `${digit}`;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      result = true;
+      break;
+    }
+  }
+  return result;
 }
 
 /**
